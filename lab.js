9bc1e21f3f1003cf82a9aaf32d29e929c5a45f0f
Stephanie Wilks <wilksstephanie.career@outlook.com>
2022-06-22 18:21:19 -0400

    first commit

--- lab.js
+++ lab.js
@@ -47,8 +47,6 @@ if(amysBirthYear === brittanisBirthYear){
 
 ////////// PROBLEM 4 //////////
 
-let temperature = 55;
-let rain = true;
 
 /*
   Using an if statement and a comparison operator, create code that will log a suggestion on what type of clothes one should wear for the day, based on the above temperature and rain.
@@ -144,9 +142,8 @@ let z = 5;
 // Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
 while (z > 0){
-  console.log(z);
-  z--;
-  for(i=4; i > 0; i--){
+  for(i=z; i > 0; i--){
     console.log(i);
   }
+  z--;
 }
